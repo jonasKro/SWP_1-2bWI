4aa99ea1025ad6829fa17292f08a257da33a599a
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2022-04-06 15:16:05 +0200

    switch übung

---
+++ 2.Jahr/JAVA/src/Switch.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class Switch {
+    public static void main(String[] args){
+        Random random = new Random();
+        int randomNumber = random.nextInt(6);
+        randomNumber = randomNumber + 5;
+        System.out.println(randomNumber);
+        switch (randomNumber){
+            case 5:
+                System.out.println("fünf");
+                break;
+            case 6:
+                System.out.println("sechs");
+                break;
+            case 7:
+                System.out.println("sieben");
+                break;
+            case 8:
+                System.out.println("acht");
+                break;
+            case 9:
+                System.out.println("neun");
+                break;
+            case 10:
+                System.out.println("zehn");
+                break;
+        }
+    }
+
+
+
+
+}
