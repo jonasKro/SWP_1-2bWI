335677bc53285d0fcdd9fada37f6db1d2bcd95f7
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2022-04-06 14:51:41 +0200

    IF übung gefixt

--- 2.Jahr/JAVA/src/If_übung1.java
+++ 2.Jahr/JAVA/src/If_übung1.java
@@ -1,8 +1,7 @@
-package at.ran.basics;
 
 import java.util.Random;
 
-public class Main {
+public class If_übung1 {
     public static void main(String[] args) {
         // Erstelle eine Zufallszahl zwischen 0 und 100
         Random random = new Random();
@@ -15,7 +14,7 @@ public class Main {
         // Wenn die Zahl größer als 50 ist gib aus "Large"
             if (randomNumber < 20 ){
                 System.out.println("mini");
-            } else if ( randomNumber <20 && randomNumber > 50){
+            } else if ( randomNumber > 20 && randomNumber < 50 ){
                 System.out.println("medium");
             }else  {
                 System.out.println("large");
--- 2.Jahr/JAVA/src/Main.java
+++ 2.Jahr/JAVA/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
             // Wenn die Zahl größer als 50 ist gib aus "Large"
             if (randomNumber < 20 ){
                 System.out.println("mini");
-            } else if ( (randomNumber <20) && (randomNumber > 50)){
+            } else if ( (randomNumber >20) && (randomNumber < 50)){
                 System.out.println("medium");
             }else  {
                 System.out.println("large");
