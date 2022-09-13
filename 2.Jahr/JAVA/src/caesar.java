b5409b2fc8c2e94444229dd39e0c5d6e8ddb89bc
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2022-09-13 10:54:28 +0200

    Caesar verschlüsselung fertig

---
+++ 2.Jahr/JAVA/src/caesar.java
@@ -0,0 +1,19 @@
+public class caesar {
+    public static void main(String[] args){
+        int encryptioncount = 2;
+        char word[] = {'c','a','e','s','a','r'};
+        char test = 'f';
+        test += encryptioncount;
+
+        for (int i=0; i<6;i++){
+            char letter = word[i];
+            letter+= encryptioncount;
+            word[i] = letter;
+        }
+        System.out.println(word);
+
+    }
+
+}
+
+
