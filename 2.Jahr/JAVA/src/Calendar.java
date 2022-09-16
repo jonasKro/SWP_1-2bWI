dd083ff96177d4f7545b9f45f165d9182659e8eb
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2022-09-16 14:48:17 +0200

    tic tac toe angefangen

---
+++ 2.Jahr/JAVA/src/Calendar.java
@@ -0,0 +1,12 @@
+public class Calendar {
+    public static void main(String[] args){
+        char playerSelect = 'o';
+        for (int i = 0; i<3 ; i++){
+            for (int j = 0; j<3 ; j++){
+                System.out.println("|"+playerSelect+"|"+playerSelect+"|"+playerSelect+"|");
+
+            }
+            break;
+        }
+    }
+}
