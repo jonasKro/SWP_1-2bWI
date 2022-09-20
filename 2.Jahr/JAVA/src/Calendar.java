eede2c7fe9b70ec9b3ba3015e7bf1e6c22d2dc5d
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2022-09-20 10:51:57 +0200

    Kalender angefangen

--- 2.Jahr/JAVA/src/Calendar.java
+++ 2.Jahr/JAVA/src/Calendar.java
@@ -1,12 +1,37 @@
+import java.util.Scanner;
+
 public class Calendar {
     public static void main(String[] args){
-        char playerSelect = 'o';
-        for (int i = 0; i<3 ; i++){
+        int month = 0;
+        int day =0;
+        int[] length = {};
+        String[] days= {"MO","DI","MI","DO","FR","SA","SO"};
+        Scanner input = new Scanner(System.in);
+        System.out.println("geben sie den gewünschten monat an");
+        month = input.nextInt();
+
+
+
+        switch (month){
+            case 1,3,5,7,8,10,12:
+                System.out.println("31 tage");
+                for (int i=0;i<31;i++){
+                    length[i]=i+1;
+                    System.out.println(length[i]);
+                }
+                break;
+            case 2,4,6,9,11:
+                System.out.println("30 tage");
+                break;
+        }
+
+
+
+
+
             for (int j = 0; j<3 ; j++){
-                System.out.println("|"+playerSelect+"|"+playerSelect+"|"+playerSelect+"|");
+                System.out.println("|"+days[0]+"|"+days[1]+"|"+days[2]+"|"+days[3]+"|"+days[4]+"|"+days[5]+"|"+days[6]+"|");
 
             }
-            break;
-        }
     }
 }
