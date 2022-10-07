02e8eac29c88fcacd60fa264caf51e6b7244a268
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2022-10-07 14:24:57 +0200

    tictactoe einzelspieler

--- 2.Jahr/JAVA/src/TicTacToe.java
+++ 2.Jahr/JAVA/src/TicTacToe.java
@@ -4,6 +4,11 @@ public class TicTacToe {
     public static void main(String[] args) {
         String player1select = "";
         String player2select = "";
+        int row1 = 0;
+        int row2 = 0;
+        int collumn1 = 0;
+        int collumn2 = 0;
+        boolean gamerunning = true;
         String[][] gameGrid = new String[3][3];
 
 
@@ -18,12 +23,30 @@ public class TicTacToe {
         }
         for (int i = 0; i<3;i++){
             for (int j = 0; j<3;j++){
-                System.out.print(" "+"-");
+                gameGrid[i][j] = "-";
             }
-            System.out.println(" ");
-        }
-        System.out.println("spieler 1: geben sie die gewünschte reihe ein (1-3)");
 
+        }
+        while (gamerunning = true){
+            System.out.println("spieler 1: geben sie die gewünschten koordinaten ein (1 - 3)");
+            row1 = input.nextInt() -1;
+            collumn1 = input.nextInt() -1;
+            gameGrid[row1][collumn1] = player1select;
+            System.out.println(gameGrid[row1][collumn1]);
+            for (int i = 0; i<3;i++){
+                for (int j = 0; j<3;j++){
+                    System.out.print(" "+gameGrid[i][j] );
+                }
+                System.out.println(" ");
+            }
+            if (){
+                gamerunning = false;
+            }
+        }
 
     }
 }
+
+
+
+
