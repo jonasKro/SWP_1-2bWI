1f9d0d488b0ba04ce6a5a04f1a60f6a9c1fb8585
Jonas Kronabitter <jonas.kronabitter@student.htldornbirn.at>
2022-11-29 09:30:45 +0100

    first commit

---
+++ 2.Jahr/JAVA/src/Teststation.java
@@ -0,0 +1,10 @@
+public class Teststation {
+    public static void main(String[] args){
+        int[] length = {};
+
+        for (int i=0;i < 31;i++){
+            length[i]=i+1;
+            System.out.println(length[i]);
+        }
+    }
+}
